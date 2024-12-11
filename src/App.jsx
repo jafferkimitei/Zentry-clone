a539362cb059e7864560965ccd31de3f2532db94
yunghavy <kimutaij192@gmail.com>
2024-12-11 16:05:17 +0300

    foundations

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold font'>Welcome to Awwards</h1>
+    </main>
+  )
+}
+
+export default App
